0f05273eb8661aea11313254a3281d73a17fbecc
nuanmi155 <xileoxmax@gmail.com>
2018-08-01 16:41:52 +0700

    Create Member

---
+++ SEPApplication3/SEPApplication3/Controllers/MembersController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SEPApplication3.Models;
+using SEPApplication3.Connect;
+
+namespace SEPApplication3.Controllers
+{
+    public class MembersController : Controller
+    {
+        Connect_API connect = new Connect_API();
+        private sepoopcsEntities db = new sepoopcsEntities();
+
+        // GET: Members
+        public ActionResult Index(int courseId)
+        {
+            ViewBag.CourseId = courseId;
+            var members = db.Members.Where(m => m.Course_id == courseId).ToList();
+            return View(members);
+        }
+
+        // GET: Members/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Member member = db.Members.Find(id);
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(member);
+        }
+
+        // GET: Members/Create
+        public ActionResult Create()
+        {
+            ViewBag.Course_id = new SelectList(db.Courses, "id", "Code");
+            return View();
+        }
+
+        // POST: Members/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(int courseId, Member member)
+        {
+            var result = new Connect_API().GetStudent(member.Code);
+            if (result.Code == 0)
+            {
+                foreach (var item in result.Data)
+                {
+                    member.Fullname = item.Fullname;
+                    member.Birthday = item.Birthday;
+                    member.Course_id = courseId;
+                    db.Members.Add(member);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index", new { courseId = courseId });
+            }
+
+            ViewBag.Course_id = courseId;
+            return View(member);
+        }
+
+        // GET: Members/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Member member = db.Members.Find(id);
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Course_id = new SelectList(db.Courses, "id", "Code", member.Course_id);
+            return View(member);
+        }
+
+        // POST: Members/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Code,Fullname,Birthday,Course_id")] Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(member).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Course_id = new SelectList(db.Courses, "id", "Code", member.Course_id);
+            return View(member);
+        }
+
+        // GET: Members/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Member member = db.Members.Find(id);
+            if (member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(member);
+        }
+
+        // POST: Members/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Member member = db.Members.Find(id);
+            db.Members.Remove(member);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
